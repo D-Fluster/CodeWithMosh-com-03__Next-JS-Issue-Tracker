b740e2f93edc3df145a71ec7bd16250842178a74
D-Fluster <158021342+D-Fluster@users.noreply.github.com>
2024-03-11 00:19:34 -0700

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { PiBugDuotone } from "react-icons/pi";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  // console.log(currentPath);
+  // // Works as expected, but logging each 4x to console
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -17,7 +25,11 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-700 hover:text-zinc-300 transition-colors"
+            className={classnames({
+              "text-emerald-500": link.href === currentPath,
+              "text-zinc-700": link.href !== currentPath,
+              "hover:text-zinc-300 transition-colors": true,
+            })}
             href={link.href}
           >
             {link.label}
@@ -91,12 +103,12 @@ I've chosen the "PiBugDuotone" icon from the "pi" library
 // so I add the following import line and replace the
 // "Logo" placeholder text with "<PiBugDuotone />":
 
-import { PiBugDuotone } from "react-icons/pi";
+    import { PiBugDuotone } from "react-icons/pi";
 
 Note that Mosh uses "<AiFillBug />" in his tutorial, which
 // is imported with this line, as it's in the "ai" library:
 
-import { AiFillBug } from "react-icons/ai";
+    import { AiFillBug } from "react-icons/ai";
 
 At this phase, we are manually listing LI elements to be
 // navigated to in an UL as follows:
@@ -148,5 +160,76 @@ Updated in "return" portion of our function -- note that
 // STYLING THE ACTIVE LINK
 // https://youtu.be/J9sfR6HN6BY?t=1195
 // 
+
+First, we add a new "issues" folder inside the "app" folder
+// with a "page.tsx" file to create the "/issues" page
+
+To get the current "pass" or "route," we have to use one of
+// the "hooks" that come with Next.js, usePathname:
+
+    import { usePathname } from "next/navigation";
+
+After importing we define a new constant variable
+// "currentPath" using the "usePathname" hook, again before
+// the "return" portion of our function:
+
+    const currentPath = usePathname();
+
+However, this hook is dependent on browser APIs, so in
+// order to use this hook in this component, we have to 
+// convert it to a client component -- because we can only 
+// access browser APIs within client components
+
+Thus, we also add the "use client"; directive at the top
+
+To style the active link differently, we need to add a 
+// conditional statement that checks if the current path
+// of the <Link> element is equivalent to the current href:
+
+    className={`
+        ${link.href === currentPath ? 
+            "text-emerald-500" : "text-zinc-700"}
+        hover:text-zinc-300 transition-colors`}
+
+Note that we use back-ticks ` instead of quotes " here in
+// order to make dynamic additions/differentiations -- and
+// again Mosh uses zinc-900 for the active link, while 
+// I've chosen emerald-500
+
+This is a simple method for styling the active link in the
+// navigation bar; however, with more complex applications, 
+// we may end up with complicated condition statements and
+// it can become difficult to keep track of which classes
+// are rendered
+
+For these cases, we can use a popular package called
+// "ClassNames" using "npm i classnames@2.3.2" in Terminal
+
+Then, we import the package at the top using:
+
+    import classnames from "classnames";
+
+This is a function that we call with a given object; in the
+// object, we specify the classes that we want to render and
+// the conditions under which they should be rendered; then,
+// this function will return a string containing the classes
+// that we want to render (based on the argument object?)
+
+So instead of the long <Link className> above, we call the
+// classnames function and pass it an object with key-value
+// pairs, where the keys are the CSS classes and the values
+// should equate to either "true" or "false" and represent
+// the conditions under which those classes are rendered:
+
+    className=
+        {classnames({
+            "text-emerald-500": link.href === currentPath,
+            "text-zinc-700": link.href !== currentPath,
+            "hover:text-zinc-300 transition-colors": true,
+        })}
+
+This eliminates our string manipulation and make our code
+// cleaner and easier to read -- i.e., we can easily see
+// what classes are rendered under what conditions!
 
 */
---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IssuesPage = () => {
+  return (
+    <>
+      <div>This is the Issues Page!</div>
+    </>
+  );
+};
+
+export default IssuesPage;
